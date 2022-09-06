8f203d04c7fa5fc7361689af0d3c75896361ab22
hamzabely1 <hamzaciao80@gmail.com>
2022-09-06 13:17:10 +0200

    dernier détail

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Netflix',
+      title: 'Nextflix',
       home: HomePage(),
      routes: {
 Film_detail.routeName:(context)=>Film_detail()
